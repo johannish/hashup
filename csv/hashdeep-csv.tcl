7f593f603d3efc87cfd94bd901a63ab76397a9a9
Johann <raztus@gmail.com>
2017-07-03 17:23:49 -0600

    Handle (mostly) arbitrary CSV column names; one column must be the md5 hash. More robust error handling in parseCsv

--- csv/hashdeep-csv.tcl
+++ csv/hashdeep-csv.tcl
@@ -1,42 +1,33 @@
 namespace eval ::hashdeepCsv {}
 
 proc ::hashdeepCsv::parseCsv {hashdeepOutputCsv} {
-	set i 0
 	set hashmap {}
-	foreach line [split $hashdeepOutputCsv \n] {
+	set lines [split $hashdeepOutputCsv \n]
+
+	if {![string match "%%%% HASHDEEP*" [lindex $lines 0]]} {
+		error "Invalid input file: found [lindex $lines 0] at line 1, expected '%%%% HASHDEEP...'";
+	}
+
+	set columnInfo [regsub {^%%%% } [lindex $lines 1] {}]
+	set columns [split $columnInfo ,]
+	if {[lsearch $columns "md5"] < 0} {
+		error "Arbitrary limitation of this program: each csv line must contain an md5 hash"
+	}
+
+	foreach line $lines {
 		set firstchar [string index $line 0]
 		if {$firstchar == "%" || $firstchar == "#"} {
 			continue
 		}
 
-		#if { $i > 100 } {
-		#	break
-		#}
-
 		set parts [split $line ,]
-		#TODO: get these names dynamically from the hashdeep header
-		set size [lindex $parts 0]
-		set md5 [lindex $parts 1]
-		set sha1 [lindex $parts 2]
-		set filename [lindex $parts 3]
+		set keyValPairs {}
 
-		# The point is to compare two files, not a single one. Given that goal,
-		# a dict key of one of the file's hashes is ideal. This will not be ideal for finding
-		# duplicates within a single file.
-		# although...
-		#if {[dict exists $hashmap $md5] && $md5 != "45b47db11f92bf5fd98d41034e74c9eb"} {
-		#	puts stderr "duplicate: $md5"
-		#}
+		for {set i 0} {$i < [llength $columns]} {incr i} {
+			dict append keyValPairs [lindex $columns $i] [lindex $parts $i]
+		}
 
-		#TODO: find a better way to create this dict
-		#dict set hashmap $md5 "size $size md5 $md5 sha1 $sha1 filename $filename"
-		dict set hashmap $md5 size $size
-		dict set hashmap $md5 md5 $md5
-		dict set hashmap $md5 sha1 $sha1
-		dict set hashmap $md5 size $size
-		dict set hashmap $md5 filename "$filename"
-
-		incr i
+		dict set hashmap [dict get $keyValPairs md5] $keyValPairs
 	}
 
 	return $hashmap
--- test/hashdeep-csv.test.tcl
+++ test/hashdeep-csv.test.tcl
@@ -6,12 +6,13 @@ source ../csv/hashdeep-csv.tcl
 } -setup {
 } -body {
 	set file1Md5 f441b34188a3a5e8ad0fcfa18eb81298
+	set file1Sha1 fake-sha1
 	set csvOutput "%%%% HASHDEEP-1.0
 %%%% size,md5,sha1,filename
 ## Invoked from: /media/moo
 ## $ hashdeep -r -c md5 .
 ##
-2761139,$file1Md5,fake-sha1,/media/moo/file.txt
+2761139,$file1Md5,$file1Sha1,/media/moo/file.txt
 4034402,d3bd60842b030b94c168c2e730bb26e6,fake-sha1,/media/moo/file2.txt"
 	
 	set result [::hashdeepCsv::parseCsv $csvOutput]
@@ -19,6 +20,55 @@ source ../csv/hashdeep-csv.tcl
 	return [expr {
 		[dict get $file1 filename] == {/media/moo/file.txt}
 		&& [dict get $file1 md5] == $file1Md5
+		&& [dict get $file1 sha1] == $file1Sha1
+	}]
+} -cleanup {
+} -result 1
+
+::tcltest::test parseCsv_arbitraryColumns {
+} -setup {
+} -body {
+	set file1Md5 f441b34188a3a5e8ad0fcfa18eb81298
+	set csvOutput "%%%% HASHDEEP-1.0
+%%%% md5,size,filename
+## Invoked from: /media/moo
+## $ hashdeep -r -c md5 .
+##
+$file1Md5,1234,/media/moo/file.txt
+d3bd60842b030b94c168c2e730bb26e6,4567,/media/moo/file2.txt"
+	
+	set result [::hashdeepCsv::parseCsv $csvOutput]
+	set file1 [dict get $result $file1Md5]
+	return [expr {
+		[dict get $file1 filename] == {/media/moo/file.txt}
+		&& [dict get $file1 md5] == $file1Md5
+	}]
+} -cleanup {
+} -result 1
+
+::tcltest::test parseCsv_returnsErrorForInvalidOutput {
+} -setup {
+} -body {
+	set file1Md5 f441b34188a3a5e8ad0fcfa18eb81298
+	set csvOutput "%%%% BLAH
+%%%% size,md5,sha1,filename"
+	
+	catch {::hashdeepCsv::parseCsv $csvOutput} result options
+	return [dict get $options -code]
+} -cleanup {
+} -result 1
+
+::tcltest::test parseCsv_outputMustContainMd5 {
+} -setup {
+} -body {
+	set file1Md5 f441b34188a3a5e8ad0fcfa18eb81298
+	set csvOutput "%%%% HASHDEEP-1.0
+%%%% size,sha1,filename"
+	
+	catch {::hashdeepCsv::parseCsv $csvOutput} result options
+	return [expr {
+		[dict get $options -code] == 1
+		&& [string match "*Arbitrary limitation*" [dict get $options -errorinfo]]
 	}]
 } -cleanup {
 } -result 1
