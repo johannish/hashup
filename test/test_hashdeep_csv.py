121684c28b9075df167531e56a209aae7b7047eb
Johann <>
2020-06-13 19:23:19 -0400

    methods to read and parse header from hashdeep file

--- test/test_hashdeep_csv.py
+++ test/test_hashdeep_csv.py
@@ -1,6 +1,23 @@
 import unittest
+
 from util.hashdeep_csv import parse_header
+from util.hashdeep_csv import read_header
 
 class HashdeepCsvTest(unittest.TestCase):
-	def test_parse_header_happypath(self):
-		self.assertEqual(parse_header('moo'), '')
+	def test_parse_header(self):
+		headerlines = [
+			'%%%% HASHDEEP-1234.abcde'
+			,'%%%% size,hash1,hash2,filename'
+			,'## Invoked from: /moo/cow/place/'
+			,'## $ hashdeep -c hash1,hash2 -r .'
+			,'##'
+		]
+		result = parse_header(headerlines)
+		self.assertEqual(result.hashdeep_version, 'HASHDEEP-1234.abcde')
+		self.assertEqual(result.csv_column_names, 'size,hash1,hash2,filename')
+		self.assertEqual(result.invoked_dir, '/moo/cow/place/')
+		self.assertEqual(result.invoked_cmd, '$ hashdeep -c hash1,hash2 -r .')
+
+	def test_read_header(self):
+		results = read_header('test/data/example.hashdeep')
+		self.assertEqual(len(results), 5)
--- util/hashdeep_csv.py
+++ util/hashdeep_csv.py
@@ -1,2 +1,45 @@
-def parse_header(hashdeepCsvOutput):
-	return ''
+import re
+
+from dataclasses import dataclass
+
+@dataclass
+class HashdeepCsvHeader:
+	hashdeep_version: str
+	csv_column_names: list
+	invoked_dir: str
+	invoked_cmd: str
+
+def parse_header(hashdeepHeaderLines):
+	# TODO: this is stupid. What's the Pythonic approach to a plain data object with optional fields?
+	header = HashdeepCsvHeader('','','','')
+
+	for line in hashdeepHeaderLines:
+		match = re.search('%%%%.*(HASHDEEP-[^\s]+)', line)
+		if match is not None:
+			header.hashdeep_version = match.group(1)
+			continue
+
+		match = re.search('%%%% (size,.*)', line)
+		if match is not None:
+			header.csv_column_names = match.group(1)
+			continue
+
+		match = re.search('## Invoked from: (.*)', line)
+		if match is not None:
+			header.invoked_dir = match.group(1) or ''
+			continue
+
+		if line.startswith('## ') and line.find('Invoked') == -1:
+			header.invoked_cmd = line.lstrip('## ')
+			continue
+
+	return header
+
+def read_header(filename):
+	with open(filename, 'r') as file:
+		lines = []
+		for line in file:
+			if not line.startswith('%') and not line.startswith('#'):
+				break
+			lines.append(line)
+	return lines
